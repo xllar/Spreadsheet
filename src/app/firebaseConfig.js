aca9a08096ed2184ad396bd413027e85d57a5875
Xllar <ovicbananas@gmail.com>
2025-02-21 23:13:29 +0100

    Updated project with fixes

--- src/app/firebaseConfig.js
+++ src/app/firebaseConfig.js
@@ -7,15 +7,17 @@ import { getFirestore, collection, addDoc, getDocs, doc, updateDoc, deleteDoc, s
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBWSRndXMgdA2xvTKLtiR5iIeNFG6sAH_U",
-  authDomain: "backend-694f9.firebaseapp.com",
-  projectId: "backend-694f9",
-  storageBucket: "backend-694f9.firebasestorage.app",
-  messagingSenderId: "469497592605",
-  appId: "1:469497592605:web:f98dfa61faf6ac2e86fff4",
-  measurementId: "G-5KPEP4EQHB"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
+
 
 // Initialize Firebase
 
